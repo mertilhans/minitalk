aef7d926a4b72ad17774f0ef093963ac48101db2
mertilhans <160387403+mertilhans@users.noreply.github.com>
2025-03-02 17:05:28 +0300

    Add files via upload

--- client.c
+++ client.c
@@ -1,64 +1,73 @@
 #include <unistd.h>
 #include <signal.h>
+#include <stdlib.h>
 
+static int g_counter = 0;
 
-static int ft_atoi(char *str)
-{
+static int ft_atoi(char *str) {
     int sign = 1;
     int res = 0;
 
     while ((*str >= 9 && *str <= 13) || (*str == 32)) 
         str++;
-    if (*str == '-')
+    if (*str == '-') 
         sign *= -1;
     if (*str == '+' || *str == '-') 
         str++;
-    while (*str >= '0' && *str <= '9')
-    {
+    while (*str >= '0' && *str <= '9') {
         res = (res * 10) + (*str - '0');
         str++;
     }
     return (res * sign);
 }
-
-void transport_signal(int s_pid, char *msg)
-{
+void signal_handler(int sig) {
+    if (sig == SIGUSR1) {
+        g_counter = 1;
+    }
+}
+void transport_signal(int s_pid, char *msg) {
     int i;
-	int j;
+    int j;
 
-	i = 0;
-    while (msg[i])
-    {
+    i = 0;
+    while (msg[i]) {
         j = 7;
-		while(j >= 0)
-		{
-			if ((msg[i] >> j ) & 1)
-				kill(s_pid , SIGUSR1);
-			else
-				kill(s_pid, SIGUSR2);
-			usleep(500);
-			j--;
-		}
-		i++;
+        while (j >= 0) {
+            if ((msg[i] >> j) & 1) {
+                kill(s_pid, SIGUSR1);  
+            } else {
+                kill(s_pid, SIGUSR2);
+            }
+            while (g_counter == 0) {
+                pause(); 
+			usleep(30 + 1);
+            }
+            g_counter = 0;
+            j--;
+        }
+        i++;
     }
 }
 
-int main(int ac, char **av)
-{
-	int s_pid;
+int main(int ac, char **av) {
+    int s_pid;
 
-	if (ac == 3)
+    if (ac == 3) 
 	{
-		s_pid = ft_atoi(av[1]);
-		if(s_pid <= 0)
-			write(1,"undefined pid \n",16);
-		transport_signal(s_pid , av[2]);
-		transport_signal(s_pid , "\n");
-	}
-	else
+        s_pid = ft_atoi(av[1]);
+        if (s_pid <= 0) {
+            write(1, "undefined pid \n", 16);
+            exit(1);
+        }
+        signal(SIGUSR1, signal_handler);
+
+        transport_signal(s_pid, av[2]);
+        transport_signal(s_pid, "\n");
+    } 
+	else 
 	{
-		write(1," ERROR = Read The Manuel\n" ,25);
-		return(1);
-	}
-	return 0;
+        write(1, "ERROR = Read The Manual\n", 25);
+        return 1;
+    }
+    return 0;
 }
--- server.c
+++ server.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include <signal.h>
 
+static int counter = 0;
+
 static void ft_putnbr(int n)
 {
     if (n > 9)
@@ -8,32 +10,39 @@ static void ft_putnbr(int n)
     write(1, &"0123456789"[n % 10], 1);
 }
 
-void handle_signal(int sig)
+void handle_signal(int sig, siginfo_t *info, void *ucontext)
 {
-	static unsigned char msg = 0; 
-    static int count = 0;  
+    static unsigned char msg = 0;
+    static int count = 0;
+
+    (void)ucontext; 
 
     if (sig == SIGUSR1)
         msg |= (1 << (7 - count));  
     count++;
-
     if (count == 8)
     {
         write(1, &msg, 1); 
         msg = 0;  
         count = 0;  
     }
+    kill(info->si_pid, SIGUSR1);
 }
 
 int main(void)
 {
-	int pid;
-	pid = getpid();
-	signal(SIGUSR1, handle_signal);
-	signal(SIGUSR2, handle_signal);
-	ft_putnbr(pid);
-	write(STDOUT_FILENO , "\n" ,1);
-	while(1337)
-		pause();
-	return 0;
+    int pid;
+    pid = getpid();
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_sigaction = handle_signal;
+    sa.sa_flags = SA_SIGINFO;
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
+    ft_putnbr(pid);
+    write(STDOUT_FILENO , "\n" ,1);
+    while (1)
+        pause(); 
+
+    return 0;
 }
